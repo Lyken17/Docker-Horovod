a1a7697a30877fc5a02f803b707304ca975b8f79
Ligeng Zhu <Lyken17@users.noreply.github.com>
2021-09-09 12:17:20 +0800

    Update openmpi-4.1.1.sh

--- openmpi-4.1.1.sh
+++ openmpi-4.1.1.sh
@@ -3,21 +3,24 @@
 ver=4.1
 version=4.1.1
 
-# apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
-#         build-essential \
-#         cmake \
-#         g++-4.8 \
-#         git \
-#         curl \
-#         vim \
-#         wget \
-#         ca-certificates \
-#         ibverbs-providers
+apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
+        build-essential \
+        cmake \
+        gcc \
+        g++ \
+        llvm \
+        clang \
+        llvm \
+        git \
+        curl \
+        vim \
+        wget \
+        ca-certificates
         
 
 # compile and install 
-mkdir /tmp/openmpi && \
-    cd /tmp/openmpi && \
+mkdir -p /root/lib/openmpi && \
+    cd /root/lib/openmpi && \
     wget https://www.open-mpi.org/software/ompi/v$ver/downloads/openmpi-$version.tar.gz && \
     tar zxf openmpi-$version.tar.gz && \
     cd openmpi-$version && \
@@ -25,4 +28,4 @@ mkdir /tmp/openmpi && \
     make -j $(nproc) all && \
     make install && \
     ldconfig && \
-    rm -rf /tmp/openmpi
+    rm -rf /root/lib/openmpi
