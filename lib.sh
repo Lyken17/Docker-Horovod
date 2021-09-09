a1eebe1af7f1e4b6d35a59c66f13a9c999885cdc
Ligeng Zhu <Lyken17@users.noreply.github.com>
2021-09-09 17:26:21 +0800

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 # build esentials 
 apt-get update -y && apt upgrade -y
-apt-get install -y zip, wget, git, vim, htop, tree, tmux, bash, zsh
+apt-get install -y zip wget git vim htop tree tmux bash zsh
 apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
         build-essential \
         cmake \
