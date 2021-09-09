8b3c0ce3e7216a7140b8653f349101fa01c98847
Ligeng Zhu <Lyken17@users.noreply.github.com>
2021-09-09 12:06:55 +0800

    Update openmpi-4.1.1.sh

--- openmpi-4.1.1.sh
+++ openmpi-4.1.1.sh
@@ -1,30 +1,26 @@
-apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
-        build-essential \
-        cmake \
-        g++-4.8 \
-        git \
-        curl \
-        vim \
-        wget \
-        ca-certificates \
-        libcudnn7=${CUDNN_VERSION} \
-        libnccl2=${NCCL_VERSION} \
-        libnccl-dev=${NCCL_VERSION} \
-        libjpeg-dev \
-        libpng-dev \
-        python${PYTHON_VERSION} \
-        python${PYTHON_VERSION}-dev \
-        librdmacm1 \
-        libibverbs1 \
-        ibverbs-providers
+# /bin/bash 
+
+ver=4.1
+version=4.1.1
+
+# apt-get update && apt-get install -y --allow-downgrades --allow-change-held-packages --no-install-recommends \
+#         build-essential \
+#         cmake \
+#         g++-4.8 \
+#         git \
+#         curl \
+#         vim \
+#         wget \
+#         ca-certificates \
+#         ibverbs-providers
         
 
 # compile and install 
 mkdir /tmp/openmpi && \
     cd /tmp/openmpi && \
-    wget https://www.open-mpi.org/software/ompi/v4.1/downloads/openmpi-4.1.1.tar.gz && \
-    tar zxf openmpi-4.0.0.tar.gz && \
-    cd openmpi-4.0.0 && \
+    wget https://www.open-mpi.org/software/ompi/v$ver/downloads/openmpi-$version.tar.gz && \
+    tar zxf openmpi-$version.tar.gz && \
+    cd openmpi-$version && \
     ./configure --enable-orterun-prefix-by-default && \
     make -j $(nproc) all && \
     make install && \
